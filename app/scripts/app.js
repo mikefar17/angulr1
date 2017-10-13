52107fa25cf624b8991f125ffee19e9f01f07ec8
Michael Farias <michaelfarias75@gmail.com>
2017-10-13 03:52:16 -0700

    Finished basic setup

---
+++ app/scripts/app.js
@@ -0,0 +1,19 @@
+(function() {
+    function config($locationProvider, $stateProvider) {
+        $locationProvider
+            .html5Mode({
+                enabled: true,
+                requireBase: false
+            });
+        $stateProvider
+            .state('home', {
+                url:'/',
+                controller: 'HomeCtrl as home',
+                templateUrl: '/templates/home.html'
+            });
+    }
+    
+    angular
+        .module('angular1-project', ['ui.router', 'firebase'])
+        .config(config);
+})();
