c510fa6a713b7d2b4f6405124077fb1974f28fc3
Michael Farias <michaelfarias75@gmail.com>
2017-11-13 13:46:23 -0800

    Add Empty Rooms

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -2,9 +2,14 @@
     function HomeCtrl(Room) {
         var home =this;
         home.rooms = Room.all;
-    }
+        home.currentRoom = null;
+        
+        home.setCurrentRoom = function(room) {
+            home.currentRoom = room;
+        }
+    }   
 
     angular
         .module('angular1-project')
-        .controller('HomeCtrl', ['Room',HomeCtrl]);
+        .controller('HomeCtrl', ['Room', HomeCtrl]);
 })();
